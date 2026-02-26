04d562b98464123782a7eed200bbe60af0d96359
Kirubashini-R-B <kirubashiniramesh@gmail.com>
2026-02-26 18:18:03 +0530

    Update docx.bash

--- docx.bash
+++ docx.bash
@@ -41,3 +41,5 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
 
+git config --global http.proxy http://klm:TechnicHERC2026_@117.232.103.173:3128
+git config --global https.proxy http://klm:TechnicHERC2026_@117.232.103.173:3128
